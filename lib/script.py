0499fa8e3eea10ab060d3c6a9baebe8ae16be3d5
CV2Tre <cvlookin@me.com>
2024-03-19 14:04:48 -0400

    done

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,37 @@
+import random
 from capitals import states
 
-print(states)
+# print(states)
+
+def play_game():
+    print('Heyy, This is the State Capital Guessing Game!/n')
+    score = 0
+
+states_copy = states[:]
+
+while states_copy:
+    state_obj = random.choice(states_copy)
+    state = state_obj['name']
+    capital = state_obj['capital']
+    states_copy.remove(state_obj)
+
+    guess = input(f'What is the Capital of {state}? ').strip().title()
+
+if guess == capital:
+      print('Correct!')
+      score =+ 1 
+else: 
+      print(f'Incorrect. The Capital of {state} is {capital}.')
+
+print(f"/nYou scored {score} out of {len(states)}.")
+
+
+play_again = input('Do you want to play again? (yes/no) ').strip().lower()
+if play_again == 'yes':
+    play_game()
+else:
+    print('Thanks for playing!')
+
+
+play_game()
+      
